7ed366aa2e52f93a0aae3af9bd7454accb4670c8
Negera Wakgari Deresa <u0113965@kuleuven.be>
2021-12-27 22:02:34 +0100

    I have made changes to the previous version

---
+++ Web.R
@@ -0,0 +1,7 @@
+
+install.packages("blogdown")   # install the blogdown package
+library(blogdown)              # load blogdown
+new_site(theme = "wowchemy/starter-academic")  # create your website!
+file.edit("gitignore")
+blogdown::check_site()
+blogdown::config_Rprofile()
